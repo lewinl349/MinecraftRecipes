6048b35c23d992818082d2f22e243ab62c2863d1
lewin349 <lewinl349@gmail.com>
2024-01-03 22:45:56 -0500

    Add ability to read shaped recipes

--- getItem.js
+++ getItem.js
@@ -1,16 +1,54 @@
 const fs = require("fs");
 
-getRecipe("acacia_boat");
+// This only gets recipes from shaped crafting. (Have to check for smelting, shapeless, etc...)
+
+//const recipe = getRecipe("acacia_boat");
+const recipe = getRecipe("piston");
+
+for (x of recipe) {
+    console.log(x);
+}
 
 function getRecipe(item) {
     const recipeObj = JSON.parse(readFile(item));
 
-    console.log(recipeObj.pattern);
+    return convertToArray(recipeObj.pattern, getIngredients(recipeObj.key));
+}
+
+function getIngredients(data) {
+    const ingredientSymbols = {};
+
+    for (symbol in data) {
+        for (item in data[symbol]) {
+            ingredientSymbols[symbol] = data[symbol][item];
+        }      
+    }
+
+    //console.log(ingredientSymbols);
+    return ingredientSymbols;
 }
 
 function readFile(item) {
-    let fileName = item + ".json";
+   const fileName = item + ".json";
 
     return fs.readFileSync(fileName, "utf-8");
+}
+
+function convertToArray(pattern, ingredientSymbols) {
+    const recipe = [];
+
+    for (let i = 0; i < pattern.length; i++) {
+        recipe.push([]);
+        for (letter of pattern[i]) {
+            if (letter == " ") {
+                recipe[i].push("");
+            }
+            else {
+                recipe[i].push(ingredientSymbols[letter]);
+            }
+        }
+    }
+
+    return recipe;
 }
 
